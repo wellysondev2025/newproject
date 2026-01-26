2daa80b5c82e60695f48a9056c1f0eca7c55dddf
Wellyson <wellysonequipeasd@gmail.com>
2026-01-26 13:50:20 -0300

    feat: user registration endpoint

---
+++ backend/users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("email", "name", "password")
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User.objects.create_user(
+            password=password,
+            **validated_data
+        )
+        return user
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import UserRegisterSerializer
+
+
+class UserRegisterView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Usuário criado com sucesso"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
